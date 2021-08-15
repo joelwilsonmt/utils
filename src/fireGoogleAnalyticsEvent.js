4336e44dca5c19ffe89b4fa5124c94c04f89c41c
Joel Wilson <joel.wilson@submittable.com>
2021-08-15 13:59:24 -0600

    Update fireGoogleAnalyticsEvent.js

--- src/fireGoogleAnalyticsEvent.js
+++ src/fireGoogleAnalyticsEvent.js
@@ -1,6 +1,6 @@
 export default ({ category = "User", action, label, value, isConversion, noInteraction }) => {
     // console.log("action fired", category, action, label, value)
-    if (window?.gtag) {
+    if (window && window.gtag) {
         window.gtag('event', action, {
             'event_category': category,
             'event_label': label,
